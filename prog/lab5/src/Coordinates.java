fbbe922347811309004dff8faab01a78ebc0a482
korlir <anonym.show@mail.ru>
2022-01-30 23:27:11 +0300

    add classes from task

---
+++ prog/lab5/src/Coordinates.java
@@ -0,0 +1,4 @@
+public class Coordinates {
+    private float x; //Значение поля должно быть больше -407
+    private Integer y; //Поле не может быть null
+}
---
+++ prog/lab5/src/Government.java
@@ -0,0 +1,7 @@
+public enum Government {
+    KRITARCHY,
+    MATRIARCHY,
+    OLIGARCHY,
+    PLUTOCRACY,
+    REPUBLIC;
+}
