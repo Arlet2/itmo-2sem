23651ff1344d8d5af358a1864865949462c2b0a6
korlir <anonym.show@mail.ru>
2022-02-19 03:29:36 +0300

    Add validation for args in main

--- prog/lab5/src/Main.java
+++ prog/lab5/src/Main.java
@@ -1,9 +1,22 @@
 import commands.CommandController;
 import data_control.DataController;
+import exceptions.IncorrectArgumentException;
+import exceptions.MissingArgumentException;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            argsValidator(args);
+        } catch (MissingArgumentException | IncorrectArgumentException e) {
+            System.out.println("Ошибка инициализации коллекции: "+e.getMessage());
+        }
         CommandController cc = new CommandController(new DataController(args[0]));
         cc.listenConsole();
+    }
+    private static void argsValidator (final String[] args) throws MissingArgumentException, IncorrectArgumentException {
+        if(args.length == 0)
+            throw new MissingArgumentException("отсутствует имя файла");
+        if(args.length > 1)
+            throw new IncorrectArgumentException("слишком много аргументов");
     }
 }
